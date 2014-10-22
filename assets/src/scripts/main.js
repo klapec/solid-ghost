25dc4567f14378477775f0c50b4aafb9f7342612
Andrzej Kłapeć <andrzej.klapec@gmail.com>
2014-10-22 23:27:53 +0200

    Different header animation for mobile

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
   hljs.initHighlightingOnLoad();
+
+  var width = $(window).width();
+
+  if (width <= 900) {
+    $('.animated').removeClass('fadeInLeft').addClass('fadeInDown');
+  }
   
 });
