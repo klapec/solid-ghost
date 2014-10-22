418fc4ea03ff8c9abb92862e4bd60578d375414f
Andrzej Kłapeć <andrzej.klapec@gmail.com>
2014-10-22 23:38:21 +0200

    Got rid of animations

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
   hljs.initHighlightingOnLoad();
-
-  var width = $(window).width();
-
-  if (width <= 900) {
-    $('.animated').removeClass('fadeInLeft').addClass('fadeInDown');
-  }
   
 });
